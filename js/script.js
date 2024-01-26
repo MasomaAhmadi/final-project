1358694852e0291d94201cc7f36522021d5321cf
Masoma-Ahmadi <masomaahmadi2022@gmail.com>
2024-01-26 14:17:58 -0800

    make full responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const menu = document.getElementById('menu')
 const nav = document.getElementById('mobile-nav')
+const closeMenu = document.getElementById('close')
 
 menu.addEventListener('click', () => {
-    nav.classList.toggle('hidden')
+    nav.classList.remove('hidden')
+})
+
+closeMenu.addEventListener('click', () => {
+    nav.classList.add('hidden')
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
-  theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        "BK": "url(./node-modules/BK.jpg)"
-      }),
-      
-    },
-  },
-  plugins: [],
-}
+	content: ["./*.html"],
+	theme: {
+		extend: {
+		},
+	},
+	plugins: [],
+};
 
